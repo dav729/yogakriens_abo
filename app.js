290b7584fd30624ec38666e735b4fe145eba67b7
dav729 <dmvat@list.ru>
2021-04-25 20:39:42 +0200

    Connected json js and html
    now you can see the name in div

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
 fetch('http://yogakriens.ch/abo/students.json')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+  	
+  	let studentsArr = data.students;
+  	let str = "";
+  	let div = document.querySelector('#studentsList');
+  	studentsArr.forEach( (el,i) => {
+  		div.innerHTML = el.firstName;
+  	});
+  });
   
