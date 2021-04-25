697008880c35d639ed26d1d96629d38d8fc6f875
dav729 <dmvat@list.ru>
2021-04-25 21:14:38 +0200

    color scheme for left lessons
    removed striped
    added color scheme depended on amount of left lessons

--- app.js
+++ app.js
@@ -4,9 +4,24 @@ fetch('http://yogakriens.ch/abo/students.json')
   	let studentsArr = data.students;
   	let str = "";
   	let studentsTable = document.querySelector('#studentsTable');
+  	let aboStatus = "",
+  		needNew = "uk-alert-danger",
+  		almostFinished = "uk-alert-warning",
+  		allGood = "uk-alert-success";
   	studentsArr.forEach( (el,i) => {
+  		switch ( el.lessonsLeft ){
+  			case "1":
+  				aboStatus = needNew;
+  			break;
+  			case "2":
+  				aboStatus = almostFinished;
+  			break;
+  			default:
+  				aboStatus = allGood;
+
+  		}
   		str += 
-  		`<tr>
+  		`<tr class="${aboStatus}">
   			<td>${i+1}</td>
   			<td>${el.firstName}</td>
   			<td>${el.secondName}</td>
